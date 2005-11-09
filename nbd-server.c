ecdc049b6f72434013393e07f9af66d8bad3b612
yoe <yoe>
2005-11-09 22:19:18 +0000

    r127: Fix command line parsing.

--- nbd-server.c
+++ nbd-server.c
@@ -276,6 +276,7 @@ void usage() {
  **/
 SERVER* cmdline(int argc, char *argv[]) {
 	int i=0;
+	int nonspecial=0;
 	int c;
 	struct option long_options[] = {
 		{"read-only", no_argument, NULL, 'r'},
@@ -286,11 +287,44 @@ SERVER* cmdline(int argc, char *argv[]) {
 		{0,0,0,0}
 	};
 	SERVER *serve;
+	off_t es;
+	size_t last;
+	char suffix;
 
 	serve=g_malloc(sizeof(SERVER));
 	serve->hunksize=OFFT_MAX;
-	while((c=getopt_long(argc, argv, "a:cl:mr", long_options, &i))>=0) {
+	while((c=getopt_long(argc, argv, "-a:cl:mr", long_options, &i))>=0) {
 		switch (c) {
+		case 1:
+			switch(nonspecial++) {
+			case 0:
+				serve->port=strtol(optarg, NULL, 0);
+				break;
+			case 1:
+				serve->exportname = g_strdup(optarg);
+				if(serve->exportname[0] != '/') {
+					fprintf(stderr, "E: The to be exported file needs to be an absolute filename!\n");
+					exit(EXIT_FAILURE);
+				}
+				break;
+			case 2:
+				last=strlen(optarg)-1;
+				suffix=optarg[last];
+				if (suffix == 'k' || suffix == 'K' ||
+				    suffix == 'm' || suffix == 'M')
+					optarg[last] = '\0';
+				es = (off_t)atol(optarg);
+				switch (suffix) {
+					case 'm':
+					case 'M':  es <<= 10;
+					case 'k':
+					case 'K':  es <<= 10;
+					default :  break;
+				}
+				serve->expected_size = es;
+				break;
+			}
+			break;
 		case 'r':
 			serve->flags |= F_READONLY;
 			break;
@@ -316,36 +350,9 @@ SERVER* cmdline(int argc, char *argv[]) {
 	}
 	/* What's left: the port to export, the name of the to be exported
 	 * file, and, optionally, the size of the file, in that order. */
-	if(++i>=argc) {
+	if(nonspecial<2) {
 		usage();
-		exit(0);
-	} 
-	serve->port=strtol(argv[i], NULL, 0);
-	if(++i>=argc) {
-		usage();
-		exit(0);
-	}
-	serve->exportname = argv[i];
-	if(serve->exportname[0] != '/') {
-		fprintf(stderr, "E: The to be exported file needs to be an absolute filename!\n");
 		exit(EXIT_FAILURE);
-	}
-	if(++i<argc) {
-		off_t es;
-		size_t last = strlen(argv[i])-1;
-		char suffix = argv[i][last];
-		if (suffix == 'k' || suffix == 'K' ||
-		    suffix == 'm' || suffix == 'M')
-			argv[i][last] = '\0';
-		es = (off_t)atol(argv[i]);
-		switch (suffix) {
-			case 'm':
-			case 'M':  es <<= 10;
-			case 'k':
-			case 'K':  es <<= 10;
-			default :  break;
-		}
-		serve->expected_size = es;
 	}
 	return serve;
 }
