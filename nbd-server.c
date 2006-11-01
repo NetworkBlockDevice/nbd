f9ab95ee065713eb595d79cc18c6e54236ffce93
yoe <yoe>
2006-11-01 12:41:32 +0000

    r210: Spelling fix

--- nbd-server.c
+++ nbd-server.c
@@ -331,7 +331,7 @@ void usage() {
 	       "\t-r|--read-only\t\tread only\n"
 	       "\t-m|--multi-file\t\tmultiple file\n"
 	       "\t-c|--copy-on-write\tcopy on write\n"
-	       "\t-C|--config-file\tspecify an alternat configuration file\n"
+	       "\t-C|--config-file\tspecify an alternate configuration file\n"
 	       "\t-l|--authorize-file\tfile with list of hosts that are allowed to\n\t\t\t\tconnect.\n"
 	       "\t-a|--idle-time\t\tmaximum idle seconds; server terminates when\n\t\t\t\tidle time exceeded\n\n"
 	       "\tif port is set to 0, stdin is used (for running from inetd)\n"
