252bacbfcb9f4d2de59aef4468bd3ae66ab92e10
yoe <yoe>
2007-09-18 12:32:20 +0000

    r283: Don't dereference a command if it's empty

--- nbd-server.c
+++ nbd-server.c
@@ -1259,7 +1259,7 @@ int do_run(gchar* command, gchar* file) {
 	gchar* cmd;
 	int retval=0;
 
-	if(*command) {
+	if(command && *command) {
 		cmd = g_strdup_printf(command, file);
 		retval=system(cmd);
 		g_free(cmd);
