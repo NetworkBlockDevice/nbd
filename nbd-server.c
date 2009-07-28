c24b70e698f54d27522fd8089130c35c4147f46d
Wouter Verhelst <w@uter.be>
2009-07-28 18:00:11 +0200

    Cleanup
    - Handle all values in the enum, so that gcc -Wall doesn't dislike us
    - Code style fixes

--- nbd-server.c
+++ nbd-server.c
@@ -1655,28 +1655,29 @@ void dousers(void) {
 void glib_message_syslog_redirect(const gchar *log_domain,
                                   GLogLevelFlags log_level,
                                   const gchar *message,
-                                  gpointer user_data)
-{
-    int level=LOG_DEBUG;
+                                  gpointer user_data) {
+	int level=LOG_DEBUG;
     
-    switch( log_level )
-    {
-      case G_LOG_FLAG_FATAL:
-      case G_LOG_LEVEL_CRITICAL:
-      case G_LOG_LEVEL_ERROR:    
-        level=LOG_ERR; 
-        break;
-      case G_LOG_LEVEL_WARNING:
-        level=LOG_WARNING;
-        break;
-      case G_LOG_LEVEL_MESSAGE:
-      case G_LOG_LEVEL_INFO:
-        level=LOG_INFO;
-        break;
-      case G_LOG_LEVEL_DEBUG:
-        level=LOG_DEBUG;
-    }
-    syslog(level, message);
+	switch( log_level ) {
+		case G_LOG_FLAG_FATAL:
+		case G_LOG_LEVEL_CRITICAL:
+		case G_LOG_LEVEL_ERROR:    
+			level=LOG_ERR; 
+			break;
+		case G_LOG_LEVEL_WARNING:
+			level=LOG_WARNING;
+			break;
+		case G_LOG_LEVEL_MESSAGE:
+		case G_LOG_LEVEL_INFO:
+			level=LOG_INFO;
+			break;
+		case G_LOG_LEVEL_DEBUG:
+			level=LOG_DEBUG;
+		case G_LOG_FLAG_RECURSION:
+		case G_LOG_LEVEL_MASK:
+			break;
+	}
+	syslog(level, message);
 }
 #endif
 
