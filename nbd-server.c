ba6f1a77e02d70e1d9ae102383b70259cee677a0
Wouter Verhelst <w@uter.be>
2010-05-03 15:57:28 +0200

    Fix leaks
    There were a few strings that weren't being removed. This is
    cleanup-code that's not called all that often, but We Shouldn't
    Leak(TM).

--- nbd-server.c
+++ nbd-server.c
@@ -539,6 +539,12 @@ void remove_server(gpointer s) {
 	g_free(server->exportname);
 	if(server->authname)
 		g_free(server->authname);
+	if(server->listenaddr)
+		g_free(server->listenaddr);
+	if(server->prerun)
+		g_free(server->prerun);
+	if(server->postrun)
+		g_free(server->postrun);
 	g_free(server);
 }
 
