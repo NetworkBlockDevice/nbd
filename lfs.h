209bed87ca061d30f9ea8ed8c74688022b72ed55
Peter Korsgaard <jacmet@sunsite.dk>
2011-12-05 23:32:56 +0100

    lfs.h: fix !LFS builds
    Commit 448637fba4 (Fix 32/64 bit confusion) broke !LFS builds as it
    added the PARAM_OFFT defines inside the wrong conditional.
    
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- lfs.h
+++ lfs.h
@@ -6,14 +6,14 @@
 #define _FILE_OFFSET_BITS 64
 #ifndef _LARGEFILE_SOURCE
 #define _LARGEFILE_SOURCE
+#endif
 #define PARAM_OFFT PARAM_INT64
 #else
 #define PARAM_OFFT PARAM_INT
-#endif
+#endif /* NBD_LFS */
 #ifdef HAVE_SYNC_FILE_RANGE
 #define USE_SYNC_FILE_RANGE
 #define _GNU_SOURCE
 #endif /* HAVE_SYNC_FILE_RANGE */
-#endif /* NBD_LFS */
 
 #endif /* LFS_H */

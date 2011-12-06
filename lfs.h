NetworkBlockDevice/nbd

commit 448637fba46ae942b8200e8d709a7f33d468914f

Fix 32/64 bit confusion

lfs.h
@@ -6,6 +6,9 @@
 #define _FILE_OFFSET_BITS 64
 #ifndef _LARGEFILE_SOURCE
 #define _LARGEFILE_SOURCE
+#define PARAM_OFFT PARAM_INT64
+#else
+#define PARAM_OFFT PARAM_INT
 #endif
 #ifdef HAVE_SYNC_FILE_RANGE
 #define USE_SYNC_FILE_RANGE

nbd-server.c
@@ -249,6 +249,7 @@ typedef struct {
  **/
 typedef enum {
 	PARAM_INT,		/**< This parameter is an integer */
+	PARAM_INT64,		/**< This parameter is an integer */
 	PARAM_STRING,		/**< This parameter is a string */
 	PARAM_BOOL,		/**< This parameter is a boolean */
 } PARAM_TYPE;
@@ -763,7 +764,7 @@ GArray* parse_cfile(gchar* f, GError** e) {
 		{ "exportname", TRUE,	PARAM_STRING, 	&(s.exportname),	0 },
 		{ "port", 	TRUE,	PARAM_INT, 	&(s.port),		0 },
 		{ "authfile",	FALSE,	PARAM_STRING,	&(s.authname),		0 },
-		{ "filesize",	FALSE,	PARAM_INT,	&(s.expected_size),	0 },
+		{ "filesize",	FALSE,	PARAM_OFFT,	&(s.expected_size),	0 },
 		{ "virtstyle",	FALSE,	PARAM_STRING,	&(virtstyle),		0 },
 		{ "prerun",	FALSE,	PARAM_STRING,	&(s.prerun),		0 },
 		{ "postrun",	FALSE,	PARAM_STRING,	&(s.postrun),		0 },
@@ -799,6 +800,7 @@ GArray* parse_cfile(gchar* f, GError** e) {
 	gchar **groups;
 	gboolean bval;
 	gint ival;
+	gint64 i64val;
 	gchar* sval;
 	gchar* startgroup;
 	gint i;
@@ -830,7 +832,7 @@ GArray* parse_cfile(gchar* f, GError** e) {
 		} 
 		for(j=0;j<p_size;j++) {
 			g_assert(p[j].target != NULL);
-			g_assert(p[j].ptype==PARAM_INT||p[j].ptype==PARAM_STRING||p[j].ptype==PARAM_BOOL);
+			g_assert(p[j].ptype==PARAM_INT||p[j].ptype==PARAM_STRING||p[j].ptype==PARAM_BOOL||p[j].ptype==PARAM_INT64);
 			switch(p[j].ptype) {
 				case PARAM_INT:
 					ival = g_key_file_get_integer(cfile,
@@ -839,6 +841,15 @@ GArray* parse_cfile(gchar* f, GError** e) {
 								&err);
 					if(!err) {
 						*((gint*)p[j].target) = ival;
+					}
+					break;
+				case PARAM_INT64:
+					i64val = g_key_file_get_int64(cfile,
+								groups[i],
+								p[j].paramname,
+								&err);
+					if(!err) {
+						*((gint64*)p[j].target) = i64val;
 					}
 					break;
 				case PARAM_STRING:

commit 209bed87ca061d30f9ea8ed8c74688022b72ed55

lfs.h: fix !LFS builds
Commit 448637fba4 (Fix 32/64 bit confusion) broke !LFS builds as it
added the PARAM_OFFT defines inside the wrong conditional.

Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

lfs.h
@@ -6,14 +6,14 @@
 #define _FILE_OFFSET_BITS 64
 #ifndef _LARGEFILE_SOURCE
 #define _LARGEFILE_SOURCE
+#endif
 #define PARAM_OFFT PARAM_INT64
 #else
 #define PARAM_OFFT PARAM_INT
-#endif
+#endif /* NBD_LFS */
 #ifdef HAVE_SYNC_FILE_RANGE
 #define USE_SYNC_FILE_RANGE
 #define _GNU_SOURCE
 #endif /* HAVE_SYNC_FILE_RANGE */
-#endif /* NBD_LFS */
 
 #endif /* LFS_H */