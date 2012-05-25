ba26b9634912dbd79102484e6e0c4b27e7f704d0
Jason A. Donenfeld <Jason@zx2c4.com>
2012-05-25 04:36:17 +0200

    Initialize stack variables opts and cflags.
    Without initializing these variables, nbd-client will some of the time
    list exports instead of connecting to them, depending on the wing flaps
    of a butterfly and cosmic alien rays. The fix is to set the flags to 0
    so that nbd-client operates reliably.

--- nbd-client.c
+++ nbd-client.c
@@ -434,8 +434,8 @@ int main(int argc, char *argv[]) {
 	int nonspecial=0;
 	char* name=NULL;
 	uint32_t needed_flags=0;
-	uint32_t cflags;
-	uint32_t opts;
+	uint32_t cflags=0;
+	uint32_t opts=0;
 	struct option long_options[] = {
 		{ "block-size", required_argument, NULL, 'b' },
 		{ "check", required_argument, NULL, 'c' },
