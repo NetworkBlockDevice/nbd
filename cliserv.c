NetworkBlockDevice/nbd

commit 43b96ec0d22c0a42c20104cb66aa2d2236da3e77

Clean up cliserv.h
Previously, cliserv.h also contained some functions, which would then be
defined twice if we hadn't inlined them.

That is a fairly ugly way of doing things, so instead move the few
functions in cliserv.h to a new cliserv.c, and compile and link that in.

This should also make more recent versions of clang not be unhappy about
"missing" functions

Makefile.am
@@ -2,9 +2,9 @@ SUBDIRS = . man doc tests
 bin_PROGRAMS = nbd-server nbd-trdump
 sbin_PROGRAMS = @NBD_CLIENT_NAME@
 EXTRA_PROGRAMS = nbd-client make-integrityhuge
-nbd_client_SOURCES = nbd-client.c cliserv.h
-nbd_server_SOURCES = nbd-server.c cliserv.h lfs.h nbd.h
-nbd_trdump_SOURCES = nbd-trdump.c cliserv.h nbd.h
+nbd_client_SOURCES = nbd-client.c cliserv.h cliserv.c
+nbd_server_SOURCES = nbd-server.c cliserv.h cliserv.c lfs.h nbd.h
+nbd_trdump_SOURCES = nbd-trdump.c cliserv.h cliserv.c nbd.h
 nbd_server_CFLAGS = @CFLAGS@ @GLIB_CFLAGS@
 nbd_trdump_CFLAGS = @CFLAGS@ @GLIB_CFLAGS@
 noinst_LTLIBRARIES = libnbdsrv.la

cliserv.c
@@ -1,0 +1,74 @@
+#include <config.h>
+#include <cliserv.h>
+#include <stdio.h>
+
+void setmysockopt(int sock) {
+	int size = 1;
+#if 0
+	if (setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &size, sizeof(int)) < 0)
+		 INFO("(no sockopt/1: %m)");
+#endif
+#ifdef	IPPROTO_TCP
+	size = 1;
+	if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &size, sizeof(int)) < 0)
+		 INFO("(no sockopt/2: %m)");
+#endif
+#if 0
+	size = 1024;
+	if (setsockopt(sock, IPPROTO_TCP, TCP_MAXSEG, &size, sizeof(int)) < 0)
+		 INFO("(no sockopt/3: %m)");
+#endif
+}
+
+void err_nonfatal(const char *s) {
+	char s1[150], *s2;
+
+	strncpy(s1, s, sizeof(s1));
+	if ((s2 = strstr(s, "%m"))) {
+		strcpy(s1 + (s2 - s), strerror(errno));
+		s2 += 2;
+		strcpy(s1 + strlen(s1), s2);
+	}
+#ifndef	sun
+	/* Solaris doesn't have %h in syslog */
+	else if ((s2 = strstr(s, "%h"))) {
+		strcpy(s1 + (s2 - s), hstrerror(h_errno));
+		s2 += 2;
+		strcpy(s1 + strlen(s1), s2);
+	}
+#endif
+
+	s1[sizeof(s1)-1] = '\0';
+#ifdef ISSERVER
+	syslog(LOG_ERR, "%s", s1);
+	syslog(LOG_ERR, "Exiting.");
+#endif
+	fprintf(stderr, "Error: %s\nExiting.\n", s1);
+}
+
+void err(const char *s) {
+	err_nonfatal(s);
+	exit(EXIT_FAILURE);
+}
+
+void logging(void) {
+#ifdef ISSERVER
+	openlog(MY_NAME, LOG_PID, LOG_DAEMON);
+#endif
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+}
+
+#ifdef WORDS_BIGENDIAN
+inline u64 ntohll(u64 a) {
+	return a;
+}
+#else
+inline u64 ntohll(u64 a) {
+	u32 lo = a & 0xffffffff;
+	u32 hi = a >> 32U;
+	lo = ntohl(lo);
+	hi = ntohl(hi);
+	return ((u64) lo) << 32U | hi;
+}
+#endif

tests/run/Makefile.am
@@ -1,7 +1,7 @@
 TESTS_ENVIRONMENT=$(srcdir)/simple_test
 TESTS = cmd cfg1 cfgmulti cfgnew cfgsize write flush integrity dirconfig list #integrityhuge
 check_PROGRAMS = nbd-tester-client
-nbd_tester_client_SOURCES = nbd-tester-client.c $(top_srcdir)/cliserv.h $(top_srcdir)/netdb-compat.h
+nbd_tester_client_SOURCES = nbd-tester-client.c $(top_srcdir)/cliserv.h $(top_srcdir)/netdb-compat.h $(top_srcdir)/cliserv.c
 nbd_tester_client_CFLAGS = @CFLAGS@ @GLIB_CFLAGS@
 nbd_tester_client_CPPFLAGS = -I$(top_srcdir)
 nbd_tester_client_LDADD = @GLIB_LIBS@

cliserv.h
@@ -54,31 +54,6 @@ typedef unsigned long long u64;
 #define _FILE_OFFSET_BITS 64
 #endif
 
-static u64 cliserv_magic = 0x00420281861253LL;
-static u64 opts_magic = 0x49484156454F5054LL;
-static u64 rep_magic = 0x3e889045565a9LL;
-#define INIT_PASSWD "NBDMAGIC"
-
-#define INFO(a) do { } while(0)
-
-inline void setmysockopt(int sock) {
-	int size = 1;
-#if 0
-	if (setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &size, sizeof(int)) < 0)
-		 INFO("(no sockopt/1: %m)");
-#endif
-#ifdef	IPPROTO_TCP
-	size = 1;
-	if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &size, sizeof(int)) < 0)
-		 INFO("(no sockopt/2: %m)");
-#endif
-#if 0
-	size = 1024;
-	if (setsockopt(sock, IPPROTO_TCP, TCP_MAXSEG, &size, sizeof(int)) < 0)
-		 INFO("(no sockopt/3: %m)");
-#endif
-}
-
 #ifndef G_GNUC_NORETURN
 #if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ > 4)
 #define G_GNUC_NORETURN __attribute__((__noreturn__))
@@ -89,60 +64,21 @@ inline void setmysockopt(int sock) {
 #endif
 #endif
 
-inline void err_nonfatal(const char *s) {
-	char s1[150], *s2;
+static u64 cliserv_magic = 0x00420281861253LL;
+static u64 opts_magic = 0x49484156454F5054LL;
+static u64 rep_magic = 0x3e889045565a9LL;
+#define INIT_PASSWD "NBDMAGIC"
 
-	strncpy(s1, s, sizeof(s1));
-	if ((s2 = strstr(s, "%m"))) {
-		strcpy(s1 + (s2 - s), strerror(errno));
-		s2 += 2;
-		strcpy(s1 + strlen(s1), s2);
-	}
-#ifndef	sun
-	/* Solaris doesn't have %h in syslog */
-	else if ((s2 = strstr(s, "%h"))) {
-		strcpy(s1 + (s2 - s), hstrerror(h_errno));
-		s2 += 2;
-		strcpy(s1 + strlen(s1), s2);
-	}
-#endif
+#define INFO(a) do { } while(0)
 
-	s1[sizeof(s1)-1] = '\0';
-#ifdef ISSERVER
-	syslog(LOG_ERR, "%s", s1);
-	syslog(LOG_ERR, "Exiting.");
-#endif
-	fprintf(stderr, "Error: %s\nExiting.\n", s1);
-}
+void setmysockopt(int sock);
+void err_nonfatal(const char *s);
 
-inline void err(const char *s) G_GNUC_NORETURN;
+void err(const char *s) G_GNUC_NORETURN;
 
-inline void err(const char *s) {
-	err_nonfatal(s);
-	exit(EXIT_FAILURE);
-}
+void logging(void);
 
-inline void logging(void) {
-#ifdef ISSERVER
-	openlog(MY_NAME, LOG_PID, LOG_DAEMON);
-#endif
-	setvbuf(stdout, NULL, _IONBF, 0);
-	setvbuf(stderr, NULL, _IONBF, 0);
-}
-
-#ifdef WORDS_BIGENDIAN
-inline u64 ntohll(u64 a) {
-	return a;
-}
-#else
-inline u64 ntohll(u64 a) {
-	u32 lo = a & 0xffffffff;
-	u32 hi = a >> 32U;
-	lo = ntohl(lo);
-	hi = ntohl(hi);
-	return ((u64) lo) << 32U | hi;
-}
-#endif
+uint64_t ntohll(uint64_t a);
 #define htonll ntohll
 
 #define NBD_DEFAULT_PORT	"10809"	/* Port on which named exports are

commit 72d6ab0e7484d96608eb09242dae96020703fa57

Wire gznbd into general configure.ac/Makefile.am
Add --enable-gznbd configure option (disabled by default).
Avoid multiple recompilations of cliserv.c by factoring it into a static
library.

This fixes gznbd compilation broken after
43b96ec0d22c0a42c20104cb66aa2d2236da3e77:

gznbd.c:(.text.startup+0x420): undefined reference to `ntohll'

Makefile.am
@@ -1,15 +1,19 @@
-SUBDIRS = . man doc tests
+SUBDIRS = . man doc tests gznbd
 bin_PROGRAMS = nbd-server nbd-trdump
 sbin_PROGRAMS = @NBD_CLIENT_NAME@
 EXTRA_PROGRAMS = nbd-client make-integrityhuge
-nbd_client_SOURCES = nbd-client.c cliserv.h cliserv.c
-nbd_server_SOURCES = nbd-server.c cliserv.h cliserv.c lfs.h nbd.h
-nbd_trdump_SOURCES = nbd-trdump.c cliserv.h cliserv.c nbd.h
+noinst_LTLIBRARIES = libnbdsrv.la libcliserv.la
+libcliserv_la_SOURCES = cliserv.h cliserv.c
+libcliserv_la_CFLAGS = @CFLAGS@
+nbd_client_SOURCES = nbd-client.c cliserv.h
+nbd_server_SOURCES = nbd-server.c cliserv.h lfs.h nbd.h
+nbd_trdump_SOURCES = nbd-trdump.c cliserv.h nbd.h
 nbd_server_CFLAGS = @CFLAGS@ @GLIB_CFLAGS@
 nbd_trdump_CFLAGS = @CFLAGS@ @GLIB_CFLAGS@
-noinst_LTLIBRARIES = libnbdsrv.la
 libnbdsrv_la_SOURCES = nbdsrv.c nbdsrv.h
 libnbdsrv_la_CFLAGS = @CFLAGS@ @GLIB_CFLAGS@
-nbd_server_LDADD = @GLIB_LIBS@ libnbdsrv.la
+nbd_client_LDADD = libcliserv.la
+nbd_server_LDADD = @GLIB_LIBS@ libnbdsrv.la libcliserv.la
+nbd_trdump_LDADD = libcliserv.la
 make_integrityhuge_SOURCES = make-integrityhuge.c cliserv.h nbd.h nbd-debug.h
-EXTRA_DIST = gznbd maketr CodingStyle autogen.sh README.md
+EXTRA_DIST = maketr CodingStyle autogen.sh README.md

cliserv.c
@@ -65,11 +65,11 @@ void logging(void) {
 }
 
 #ifdef WORDS_BIGENDIAN
-inline u64 ntohll(u64 a) {
+u64 ntohll(u64 a) {
 	return a;
 }
 #else
-inline u64 ntohll(u64 a) {
+u64 ntohll(u64 a) {
 	u32 lo = a & 0xffffffff;
 	u32 hi = a >> 32U;
 	lo = ntohl(lo);

configure.ac
@@ -83,6 +83,19 @@ AC_ARG_ENABLE(
   ]
 )
 
+AC_ARG_ENABLE(
+  [gznbd],
+  [AS_HELP_STRING([--enable-gznbd],[Build gznbd too (nbd server with on-the-fly decompression of images)])],
+  [
+    AS_IF(
+      [test "x$enableval" = "xyes"],
+      [ENABLE_GZNBD=yes],
+      [ENABLE_GZNBD=no]
+    )
+  ],
+  [ENABLE_GZNBD=no]
+)
+
 AC_PROG_CC_C99
 AC_PROG_CPP
 AC_PROG_INSTALL
@@ -140,11 +153,13 @@ AC_TYPE_OFF_T
 AC_TYPE_PID_T
 nbd_server_CPPFLAGS=$nbd_server_CPPFLAGS" -DSYSCONFDIR='\"$sysconfdir\"'"
 AC_SUBST(nbd_server_CPPFLAGS)
+AM_CONDITIONAL(GZNBD, [test "x$ENABLE_GZNBD" = "xyes"])
 AC_CONFIG_HEADERS([config.h])
 AC_CONFIG_FILES([Makefile
 		 doc/Doxyfile
 		 doc/Makefile
 		 man/Makefile
+		 gznbd/Makefile
 		 tests/Makefile
 		 tests/code/Makefile
 		 tests/run/Makefile

gznbd/Makefile
@@ -1,12 +1,0 @@
-CC ?= gcc
-CFLAGS ?= -s -O2
-CFLAGS += -Wall
-CPPFLAGS += -DTRACE
-LDLIBS += -lz
-
-all: gznbd
-
-clean:
-	rm -f gznbd *.o
-
-.PHONY: all clean

gznbd/Makefile.am
@@ -1,0 +1,6 @@
+if GZNBD
+bin_PROGRAMS = gznbd
+gznbd_SOURCES = gznbd.c
+gznbd_CFLAGS = -DTRACE -Wall
+gznbd_LDADD = -lz ../libcliserv.la
+endif