35178e847d59ba83588c6208f317bc2f470d5eaf
Wouter Verhelst <w@uter.be>
2015-05-15 19:00:38 +0200

    Make it actually compile...

--- nbd-server.c
+++ nbd-server.c
@@ -1614,13 +1614,13 @@ int serveloop(GArray* servers) {
  
                         while ((pid=waitpid(-1, &status, WNOHANG)) > 0) {
                                 if (WIFEXITED(status)) {
-                                        msg(LOG_INFO, "Child exited with %d", WEXITSTATUS(status));
+                                        msg3(LOG_INFO, "Child exited with %d", WEXITSTATUS(status));
                                 }
                                 i = g_hash_table_lookup(children, &pid);
                                 if (!i) {
-                                        msg(LOG_INFO, "SIGCHLD received for an unknown child with PID %ld", (long)pid);
+                                        msg3(LOG_INFO, "SIGCHLD received for an unknown child with PID %ld", (long)pid);
                                 } else {
-                                        DEBUG("Removing %d from the list of children", pid);
+                                        DEBUG2("Removing %d from the list of children", pid);
                                         g_hash_table_remove(children, &pid);
                                 }
                         }
