cdb3fe79ee35a41b6ae03e2a2875d2ca6d7f8585
Wouter Verhelst <w@uter.be>
2015-11-06 13:43:38 +0100

    Properly handle the IPv4/IPv6 case
    What asking for "anything" using AF_UNSPEC does is horribly ill defined.
    It used to be the case that Linux would return something that would
    listen to both IPv4 and IPv6 on the same socket, but this is no longer
    the case.
    
    As such, just stop pretending and loop over multiple listening
    addresses, instead, defaulting to "::, 0.0.0.0".
    
    As a side benefit, this allows people to specify multiple addresses to
    listen to.

--- nbd-server.c
+++ nbd-server.c
@@ -2678,71 +2678,87 @@ int open_modern(const gchar *const addr, const gchar *const port,
 	int e;
         int retval = -1;
 	int sock = -1;
+	gchar** addrs;
+	gchar const* l_addr = addr;
 
-	memset(&hints, '\0', sizeof(hints));
-	hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_protocol = IPPROTO_TCP;
-	e = getaddrinfo(addr, port ? port : NBD_DEFAULT_PORT, &hints, &ai);
-	ai_bak = ai;
-	if(e != 0) {
-                g_set_error(gerror, NBDS_ERR, NBDS_ERR_GAI,
-                            "failed to open a modern socket: "
-                            "failed to get address info: %s",
-                            gai_strerror(e));
-                goto out;
+	if(!addr || strlen(addr) == 0) {
+		l_addr = "::, 0.0.0.0";
 	}
 
-	while(ai != NULL) {
-		sock = -1;
+	addrs = g_strsplit_set(l_addr, ", \t", -1);
 
-		if((sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol))<0) {
-			g_set_error(gerror, NBDS_ERR, NBDS_ERR_SOCKET,
+	for(int i=0; addrs[i]!=NULL; i++) {
+		if(addrs[i][0] == '\0') {
+			continue;
+		}
+		memset(&hints, '\0', sizeof(hints));
+		hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG;
+		hints.ai_socktype = SOCK_STREAM;
+		hints.ai_family = AF_UNSPEC;
+		hints.ai_protocol = IPPROTO_TCP;
+		e = getaddrinfo(addrs[i], port ? port : NBD_DEFAULT_PORT, &hints, &ai);
+		ai_bak = ai;
+		if(e != 0) {
+			g_set_error(gerror, NBDS_ERR, NBDS_ERR_GAI,
 				    "failed to open a modern socket: "
-				    "failed to create a socket: %s",
-				    strerror(errno));
+				    "failed to get address info: %s",
+				    gai_strerror(e));
 			goto out;
 		}
 
-		if (dosockopts(sock, gerror) == -1) {
-			g_prefix_error(gerror, "failed to open a modern socket: ");
-			goto out;
-		}
+		while(ai != NULL) {
+			sock = -1;
 
-		if(bind(sock, ai->ai_addr, ai->ai_addrlen)) {
-			/* This is so wrong. 
-			 * 
-			 * Linux will return multiple entries for the
-			 * same system when we ask it for something
-			 * AF_UNSPEC, even though the first entry will
-			 * listen to both protocols. Other systems will
-			 * return multiple entries too, but we actually
-			 * do need to open both. Sigh.
-			 *
-			 * Handle it by ignoring EADDRINUSE if we've
-			 * already got at least one socket open
-			 */
-			if(errno == EADDRINUSE && modernsocks->len > 0) {
-				goto next;
+			if((sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol))<0) {
+				g_set_error(gerror, NBDS_ERR, NBDS_ERR_SOCKET,
+					    "failed to open a modern socket: "
+					    "failed to create a socket: %s",
+					    strerror(errno));
+				goto out;
 			}
-			g_set_error(gerror, NBDS_ERR, NBDS_ERR_BIND,
-				    "failed to open a modern socket: "
-				    "failed to bind an address to a socket: %s",
-				    strerror(errno));
-			goto out;
-		}
 
-		if(listen(sock, 10) <0) {
-			g_set_error(gerror, NBDS_ERR, NBDS_ERR_BIND,
-				    "failed to open a modern socket: "
-				    "failed to start listening on a socket: %s",
-				    strerror(errno));
-			goto out;
+			if (dosockopts(sock, gerror) == -1) {
+				g_prefix_error(gerror, "failed to open a modern socket: ");
+				goto out;
+			}
+
+			if(bind(sock, ai->ai_addr, ai->ai_addrlen)) {
+				/*
+				 * Some systems will return multiple entries for the
+				 * same address when we ask it for something
+				 * AF_UNSPEC, even though the first entry will
+				 * listen to both protocols. Other systems will
+				 * return multiple entries too, but we actually
+				 * do need to open both.
+				 *
+				 * Handle this by ignoring EADDRINUSE if we've
+				 * already got at least one socket open
+				 */
+				if(errno == EADDRINUSE && modernsocks->len > 0) {
+					goto next;
+				}
+				g_set_error(gerror, NBDS_ERR, NBDS_ERR_BIND,
+					    "failed to open a modern socket: "
+					    "failed to bind an address to a socket: %s",
+					    strerror(errno));
+				goto out;
+			}
+
+			if(listen(sock, 10) <0) {
+				g_set_error(gerror, NBDS_ERR, NBDS_ERR_BIND,
+					    "failed to open a modern socket: "
+					    "failed to start listening on a socket: %s",
+					    strerror(errno));
+				goto out;
+			}
+			g_array_append_val(modernsocks, sock);
+		next:
+			ai = ai->ai_next;
 		}
-		g_array_append_val(modernsocks, sock);
-	next:
-		ai = ai->ai_next;
+		if(ai_bak) {
+			freeaddrinfo(ai_bak);
+			ai_bak=NULL;
+		}
 	}
 
         retval = 0;
