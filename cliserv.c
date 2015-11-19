d2372ec624e63e4a4e11e838ac8db56dd3ec3ef7
Robin Lee <cheeselee@fedoraproject.org>
2015-11-19 15:25:22 +0800

    Deal with end of line
    errno is not changed if read() returned 0.

--- cliserv.c
+++ cliserv.c
@@ -93,13 +93,16 @@ void readit(int f, void *buf, size_t len) {
 	ssize_t res;
 	while (len > 0) {
 		DEBUG("*");
-		if ((res = read(f, buf, len)) <= 0) {
+		res = read(f, buf, len);
+		if (res > 0) {
+			len -= res;
+			buf += res;
+		} else if (res < 0) {
 			if(errno != EAGAIN) {
 				err("Read failed: %m");
 			}
 		} else {
-			len -= res;
-			buf += res;
+			err("Read failed: End of file");
 		}
 	}
 }
