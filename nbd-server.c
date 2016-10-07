610e2e04b3ed458e698065d15fb538536e2442cc
Wouter Verhelst <w@uter.be>
2016-10-07 12:48:12 +0200

    glibc wants _DEFAULT_SOURCE rather than _BSD_SOURCE nowadays
    Signed-off-by: Wouter Verhelst <w@uter.be>

--- nbd-server.c
+++ nbd-server.c
@@ -58,6 +58,7 @@
 /* Includes LFS defines, which defines behaviours of some of the following
  * headers, so must come before those */
 #include "lfs.h"
+#define _DEFAULT_SOURCE
 #define _XOPEN_SOURCE 500 /* to get pread/pwrite */
 #define _BSD_SOURCE /* to get DT_* macros */
 #define _DARWIN_C_SOURCE /* to get DT_* macros on OS X */
