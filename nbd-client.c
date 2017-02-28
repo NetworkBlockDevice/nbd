2ccf8f8d3ca2d6e5f1101b7c04adf3fc8ca3d063
Josef Bacik <jbacik@fb.com>
2017-02-28 11:43:49 -0500

    nbd-client: add the ability to use the netlink interface
    This adds the ability to setup and tear down an nbd device with the
    netlink interface.
    
    Signed-off-by: Josef Bacik <jbacik@fb.com>
    (cherry picked from commit c72b53d7d8280ccf27e6316992f2022eb78223d7)
    [wouter: update so the patch applies in the face of STARTTLS code]
    Signed-off-by: Wouter Verhelst <w@uter.be>

--- nbd-client.c
+++ nbd-client.c
@@ -45,6 +45,14 @@
 #include <time.h>
 
 #include <linux/ioctl.h>
+
+#ifdef HAVE_NETLINK
+#include "nbd-netlink.h"
+#include <netlink/netlink.h>
+#include <netlink/genl/genl.h>
+#include <netlink/genl/ctrl.h>
+#endif
+
 #define MY_NAME "nbd_client"
 #include "cliserv.h"
 
@@ -57,6 +65,123 @@
 #endif
 
 #define NBDC_DO_LIST 1
+
+#ifdef HAVE_NETLINK
+static int callback(struct nl_msg *msg, void *arg) {
+	struct genlmsghdr *gnlh = nlmsg_data(nlmsg_hdr(msg));
+	struct nlattr *msg_attr[NBD_ATTR_MAX + 1];
+	int ret;
+	uint32_t index;
+
+	ret = nla_parse(msg_attr, NBD_ATTR_MAX, genlmsg_attrdata(gnlh, 0),
+			genlmsg_attrlen(gnlh, 0), NULL);
+	if (ret)
+		err("Invalid response from the kernel\n");
+	if (!msg_attr[NBD_ATTR_INDEX])
+		err("Did not receive index from the kernel\n");
+	index = nla_get_u32(msg_attr[NBD_ATTR_INDEX]);
+	printf("Connected /dev/nbd%d\n", (int)index);
+	return NL_OK;
+}
+
+static struct nl_sock *get_nbd_socket(int *driver_id) {
+	struct nl_sock *socket;
+
+	socket = nl_socket_alloc();
+	if (!socket)
+		err("Couldn't allocate netlink socket\n");
+
+	if (genl_connect(socket))
+		err("Couldn't connect to the generic netlink socket\n");
+	*driver_id = genl_ctrl_resolve(socket, "nbd");
+	if (*driver_id < 0)
+		err("Couldn't resolve the nbd netlink family, make sure the nbd module is loaded and your nbd driver supports the netlink interface.\n");
+	return socket;
+}
+
+static void netlink_configure(int index, int *sockfds, int num_connects,
+			      u64 size64, int blocksize, uint16_t flags,
+			      int timeout) {
+	struct nl_sock *socket;
+	struct nlattr *sock_attr;
+	struct nl_msg *msg;
+	int driver_id, i;
+
+	socket = get_nbd_socket(&driver_id);
+	nl_socket_modify_cb(socket, NL_CB_VALID, NL_CB_CUSTOM, callback, NULL);
+
+	msg = nlmsg_alloc();
+	if (!msg)
+		err("Couldn't allocate netlink message\n");
+	genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, driver_id, 0, 0,
+		    NBD_CMD_CONNECT, 0);
+	if (index >= 0)
+		NLA_PUT_U32(msg, NBD_ATTR_INDEX, index);
+	NLA_PUT_U64(msg, NBD_ATTR_SIZE_BYTES, size64);
+	NLA_PUT_U64(msg, NBD_ATTR_BLOCK_SIZE_BYTES, blocksize);
+	NLA_PUT_U64(msg, NBD_ATTR_SERVER_FLAGS, flags);
+	NLA_PUT_U64(msg, NBD_ATTR_TIMEOUT, timeout);
+
+	sock_attr = nla_nest_start(msg, NBD_ATTR_SOCKETS);
+	if (!sock_attr)
+		err("Couldn't nest the sockets for our connection\n");
+	for (i = 0; i < num_connects; i++) {
+		struct nlattr *sock_opt;
+		sock_opt = nla_nest_start(msg, NBD_SOCK_ITEM);
+		if (!sock_opt)
+			err("Couldn't nest the sockets for our connection\n");
+		NLA_PUT_U32(msg, NBD_SOCK_FD, sockfds[i]);
+		nla_nest_end(msg, sock_opt);
+	}
+	nla_nest_end(msg, sock_attr);
+
+	if (nl_send_sync(socket, msg) < 0)
+		err("Failed to setup device, check dmesg\n");
+	return;
+nla_put_failure:
+	err("Failed to create netlink message\n");
+}
+
+static void netlink_disconnect(char *nbddev) {
+	struct nl_sock *socket;
+	struct nlattr *sock_attr;
+	struct nl_msg *msg;
+	int driver_id, ret;
+
+	int index = -1;
+	if (nbddev) {
+		if (sscanf(nbddev, "/dev/nbd%d", &index) != 1)
+			err("Invalid nbd device target\n");
+	}
+	if (index < 0)
+		err("Invalid nbd device target\n");
+
+	socket = get_nbd_socket(&driver_id);
+
+	msg = nlmsg_alloc();
+	if (!msg)
+		err("Couldn't allocate netlink message\n");
+	genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, driver_id, 0, 0,
+		    NBD_CMD_DISCONNECT, 0);
+	NLA_PUT_U32(msg, NBD_ATTR_INDEX, index);
+	if (nl_send_sync(socket, msg) < 0)
+		err("Failed to disconnect device, check dmsg\n");
+	nl_socket_free(socket);
+	return;
+nla_put_failure:
+	err("Failed to create netlink message\n");
+}
+#else
+static void netlink_configure(int index, int *sockfds, int num_connects,
+			      u64 size64, int blocksize, uint16_t flags,
+			      int timeout)
+{
+}
+
+static void netlink_disconnect(char *nbddev)
+{
+}
+#endif /* HAVE_NETLINK */
 
 int check_conn(char* devname, int do_print) {
 	char buf[256];
@@ -739,7 +864,11 @@ void usage(char* errmsg, ...) {
 	} else {
 		fprintf(stderr, "%s version %s\n", PROG_NAME, PACKAGE_VERSION);
 	}
+#ifdef HAVE_NETLINK
+	fprintf(stderr, "Usage: nbd-client -name|-N name host [port] nbd_device\n\t[-block-size|-b block size] [-timeout|-t timeout] [-swap|-s] [-sdp|-S]\n\t[-persist|-p] [-nofork|-n] [-systemd-mark|-m] -L\n");
+#else
 	fprintf(stderr, "Usage: nbd-client -name|-N name host [port] nbd_device\n\t[-block-size|-b block size] [-timeout|-t timeout] [-swap|-s] [-sdp|-S]\n\t[-persist|-p] [-nofork|-n] [-systemd-mark|-m]\n");
+#endif
 	fprintf(stderr, "Or   : nbd-client -u (with same arguments as above)\n");
 	fprintf(stderr, "Or   : nbd-client nbdX\n");
 	fprintf(stderr, "Or   : nbd-client -d nbd_device\n");
@@ -772,6 +901,12 @@ void disconnect(char* device) {
 	printf("done\n");
 }
 
+#ifdef HAVE_NETLINK
+static const char *short_opts = "-A:b:c:C:d:H:hK:LlnN:pSst:uVx";
+#else
+static const char *short_opts = "-A:b:c:C:d:H:hK:lnN:pSst:uVx";
+#endif
+
 int main(int argc, char *argv[]) {
 	char* port=NBD_DEFAULT_PORT;
 	int sock, nbd;
@@ -801,6 +936,9 @@ int main(int argc, char *argv[]) {
 	bool tls = false;
 	struct sigaction sa;
 	int num_connections = 1;
+	int netlink = 0;
+	int need_disconnect = 0;
+	int *sockfds;
 	struct option long_options[] = {
 		{ "block-size", required_argument, NULL, 'b' },
 		{ "check", required_argument, NULL, 'c' },
@@ -809,6 +947,9 @@ int main(int argc, char *argv[]) {
 		{ "help", no_argument, NULL, 'h' },
 		{ "list", no_argument, NULL, 'l' },
 		{ "name", required_argument, NULL, 'N' },
+#ifdef HAVE_NETLINK
+		{ "netlink", no_argument, NULL, 'L' },
+#endif
 		{ "nofork", no_argument, NULL, 'n' },
 		{ "persist", no_argument, NULL, 'p' },
 		{ "sdp", no_argument, NULL, 'S' },
@@ -832,7 +973,7 @@ int main(int argc, char *argv[]) {
         tlssession_init();
 #endif
 
-	while((c=getopt_long_only(argc, argv, "-b:c:d:hlnN:pSst:uVC:K:A:H:x", long_options, NULL))>=0) {
+	while((c=getopt_long_only(argc, argv, short_opts, long_options, NULL))>=0) {
 		switch(c) {
 		case 1:
 			// non-option argument
@@ -885,8 +1026,9 @@ int main(int argc, char *argv[]) {
 			num_connections = (int)strtol(optarg, NULL, 0);
 			break;
 		case 'd':
-			disconnect(optarg);
-			exit(EXIT_SUCCESS);
+			need_disconnect = 1;
+			nbddev = strdup(optarg);
+			break;
 		case 'h':
 			usage(NULL);
 			exit(EXIT_SUCCESS);
@@ -895,6 +1037,11 @@ int main(int argc, char *argv[]) {
 			opts |= NBDC_DO_LIST;
 			nbddev="";
 			break;
+#ifdef HAVE_NETLINK
+		case 'L':
+			netlink = 1;
+			break;
+#endif
 		case 'm':
 			argv[0][0] = '@';
 			break;
@@ -953,6 +1100,13 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+	if (need_disconnect) {
+		if (netlink)
+			netlink_disconnect(nbddev);
+		else
+			disconnect(nbddev);
+		exit(EXIT_SUCCESS);
+	}
 #ifdef __ANDROID__
   if (swap)
     err("swap option unsupported on Android because mlockall is unsupported.");
@@ -964,7 +1118,7 @@ int main(int argc, char *argv[]) {
 					usage("no valid configuration for specified device found", hostname);
 					exit(EXIT_FAILURE);
 				}
-			} else {
+			} else if (!netlink) {
 				usage("not enough information specified, and argument didn't look like an nbd device");
 				exit(EXIT_FAILURE);
 			}
@@ -984,14 +1138,23 @@ int main(int argc, char *argv[]) {
         if (!tlshostname && hostname)
                 tlshostname = strdup(hostname);
 
+	if (netlink)
+		nofork = 1;
+
 	if(strlen(name)==0 && !(opts & NBDC_DO_LIST)) {
 		printf("Warning: the oldstyle protocol is no longer supported.\nThis method now uses the newstyle protocol with a default export\n");
 	}
 
-	if(!(opts & NBDC_DO_LIST)) {
+	if(!(opts & NBDC_DO_LIST) && !netlink) {
 		nbd = open(nbddev, O_RDWR);
 		if (nbd < 0)
 			err("Cannot open NBD: %m\nPlease ensure the 'nbd' module is loaded.");
+	}
+
+	if (netlink) {
+		sockfds = malloc(sizeof(int) * num_connections);
+		if (!sockfds)
+			err("Cannot allocate the socket fd's array");
 	}
 
 	for (i = 0; i < num_connections; i++) {
@@ -1003,6 +1166,11 @@ int main(int argc, char *argv[]) {
 			exit(EXIT_FAILURE);
 
 		negotiate(&sock, &size64, &flags, name, needed_flags, cflags, opts, certfile, keyfile, cacertfile, tlshostname, tls);
+		if (netlink) {
+			sockfds[i] = sock;
+			continue;
+		}
+
 		if (i == 0) {
 			setsizes(nbd, size64, blocksize, flags);
 			set_timeout(nbd, timeout);
@@ -1019,6 +1187,16 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
+	if (netlink) {
+		int index = -1;
+		if (nbddev) {
+			if (sscanf(nbddev, "/dev/nbd%d", &index) != 1)
+				err("Invalid nbd device target\n");
+		}
+		netlink_configure(index, sockfds, num_connections,
+				  size64, blocksize, flags, timeout);
+		return 0;
+	}
 	/* Go daemon */
 	
 #ifndef NOFORK
