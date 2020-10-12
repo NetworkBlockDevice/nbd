6366d8f967b0c721c4999f22426f214c3b9b96aa
vanshika31-design <72391615+vanshika31-design@users.noreply.github.com>
2020-10-12 11:38:28 +0530

    Create working_of_increment_and_decrement_operator.c

---
+++ working_of_increment_and_decrement_operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a= 100, b=1000;
+    float c=15.45, d=1500.45;
+    printf("++a: %d \n",++a);
+    printf("--b: %d \n",--b);
+    printf("++c: %.2f \n",++c);
+    printf("--d: %.2f \n",--d);
+    
+    return 0;
+}
