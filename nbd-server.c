NetworkBlockDevice/nbd

commit 3cb679f4a97e1a54a7285ee1f22e2888c9d3b900

nbd-server: do not straddle uses of GThreadPool across a fork()
We created the thread pool at the main initialization, before forking
off a child. This used to work just fine, but as of GLib 2.76, this no
longer works due to changes internal to the implementation of
GThreadPool.

Since we don't need to use the thread pool before the fork() call
anyway, stop trying to do so and avoid the problem altogether.

Closes: gh-146
Signed-off-by: Wouter Verhelst <w@uter.be>

nbd-server.c
@@ -199,7 +199,7 @@ char default_authname[] = SYSCONFDIR "/nbd-server/allow"; /**< default name of a
 #include <nbdsrv.h>
 
 /* Our thread pool */
-GThreadPool *tpool;
+GThreadPool *tpool = NULL;
 
 /* A work package for the thread pool functions */
 struct work_package {
@@ -3107,7 +3107,6 @@ static int mainloop_threaded(CLIENT* client) {
 		req->type = ntohl(req->type);
 		req->len = ntohl(req->len);
 
-
 		if(req->magic != htonl(NBD_REQUEST_MAGIC))
 			err("Protocol error: not enough magic.");
 
@@ -3146,9 +3145,7 @@ void destroy_pid_t(gpointer data) {
 	g_free(data);
 }
 
-static pid_t
-spawn_child(int* socket)
-{
+static pid_t spawn_child(int* socket) {
         pid_t pid;
         sigset_t newset;
         sigset_t oldset;
@@ -3231,6 +3228,7 @@ handle_modern_connection(GArray *const servers, const int sock, struct generic_c
                 }
                 /* Child just continues. */
         }
+	tpool = g_thread_pool_new(handle_request, NULL, genconf->threads, FALSE, NULL);
 
         sock_flags_old = fcntl(net, F_GETFL, 0);
         if (sock_flags_old == -1) {
@@ -3363,11 +3361,11 @@ static int append_new_servers(GArray *const servers, struct generic_conf *gencon
         int retval = -1;
 
         new_servers = parse_cfile(config_file_pos, genconf, true, gerror);
-        g_thread_pool_set_max_threads(tpool, genconf->threads, NULL);
-        if (!new_servers)
+        if(tpool) g_thread_pool_set_max_threads(tpool, genconf->threads, NULL);
+        if(!new_servers)
                 goto out;
 
-        for (i = 0; i < new_servers->len; ++i) {
+        for(i = 0; i < new_servers->len; ++i) {
                 SERVER *new_server = g_array_index(new_servers, SERVER*, i);
 
                 if (new_server->servename
@@ -3914,7 +3912,6 @@ int main(int argc, char *argv[]) {
 	}
 	if (!nodaemon)
 		daemonize();
-	tpool = g_thread_pool_new(handle_request, NULL, genconf.threads, FALSE, NULL);
 
 	setup_servers(servers, genconf.modernaddr, genconf.modernport,
 			genconf.unixsock, genconf.flags);

commit a10d07b43fe5169cd295604632faf6a08c52f75b

Also enable the thread pool when we're in inetd mode
inetd mode is initialized differently; we shouldn't forget to make the
relevant changes there, too.

In the longer term, we should probably combine these two into a single
method that just differs on how the socket is accepted, but that's more
work and not for now.

Fixes: 3cb679f4a97e1a54a7285ee1f22e2888c9d3b900
Signed-off-by: Wouter Verhelst <w@uter.be>

nbd-server.c
@@ -3948,6 +3948,7 @@ int main(int argc, char *argv[]) {
 		if(!client) {
 			exit(EXIT_FAILURE);
 		}
+		tpool = g_thread_pool_new(handle_request, NULL, genconf.threads, FALSE, NULL);
 		mainloop_threaded(client);
 		return 0;
 	}