NetworkBlockDevice/nbd

commit 29e70dc935c4e3a72d2378fce0a18e8f402585ed

Be less scary
When nothing is configured, a rather scary warning is displayed. Change
it so that it isn't quite as scary, since that produces a number of
questions from users who don't know nbd-server.

nbd-server.c
@@ -2097,8 +2097,11 @@ int main(int argc, char *argv[]) {
 	}
     
 	if(!servers || !servers->len) {
-		g_warning("Could not parse config file: %s", 
-				err ? err->message : "Unknown error");
+		if(err && !(err->domain == g_quark_from_string("parse_cfile")
+				&& err->code == CFILE_NOTFOUND)) {
+			g_warning("Could not parse config file: %s", 
+					err ? err->message : "Unknown error");
+		}
 	}
 	if(serve) {
 		g_warning("Specifying an export on the command line is deprecated.");
@@ -2106,7 +2109,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	if((!serve) && (!servers||!servers->len)) {
-		g_message("Nothing to do! Bye!");
+		g_message("No configured exports; quitting.");
 		exit(EXIT_FAILURE);
 	}
 	daemonize(serve);

commit ab4397405c1232084624a5a4b9573d9063e35aff

Remove unnecessary conditional expression
gerr is always non-null in g_warning()

Coverity-ID: 1733885
Fixes: 29e70dc ("Be less scary")
Change-Id: I3bd8e7fc8a360ebe3c5a82cde90b4542f5dd844c
Signed-off-by: Łukasz Stelmach <l.stelmach@samsung.com>

nbd-server.c
@@ -3908,8 +3908,7 @@ int main(int argc, char *argv[]) {
 	if(!servers || !servers->len) {
                 if(gerr && !(gerr->domain == NBDS_ERR
                             && gerr->code == NBDS_ERR_CFILE_NOTFOUND)) {
-			g_warning("Could not parse config file: %s",
-					gerr ? gerr->message : "Unknown error");
+			g_warning("Could not parse config file: %s", gerr->message);
 		}
 	}
 	if(serve) {