NetworkBlockDevice/nbd

commit 9aa20e2a2d3c52344e393bba6970c314934b4261

Terminate a string with a NULL byte
Coverity-ID: 1733872
Change-Id: Id18b325e6bc18a4ad0fe7dec7443c0e7790a7c8d
Signed-off-by: Łukasz Stelmach <l.stelmach@samsung.com>

nbd-client.c
@@ -699,6 +699,7 @@ void negotiate(int *sockp, u64 *rsize64, uint16_t *flags, char* name, uint32_t n
 				case NBD_REP_ERR_POLICY:
 					if(rep->datasize > 0) {
 						char errstr[1024];
+						rep->data[rep->datasize] = '\0';
 						snprintf(errstr, sizeof errstr, "Connection not allowed by server policy. Server said: %s", rep->data);
 						err(errstr);
 					} else {
@@ -709,6 +710,7 @@ void negotiate(int *sockp, u64 *rsize64, uint16_t *flags, char* name, uint32_t n
 				default:
 					if(rep->datasize > 0) {
 						char errstr[1024];
+						rep->data[rep->datasize] = '\0';
 						snprintf(errstr, sizeof errstr, "Unknown error returned by server. Server said: %s", rep->data);
 						err(errstr);
 					} else {

commit 64a66b462027c46ae8b34734c7029d3efa32fec0

Fix out-of-bounds access
Change-Id: I8b3de24a4e16e63d11da95db505d871b56f08c41
Fixes: 9aa20e2 ("Terminate a string with a NULL byte")
Signed-off-by: Łukasz Stelmach <l.stelmach@samsung.com>

nbd-client.c
@@ -703,7 +703,7 @@ void negotiate(int *sockp, u64 *rsize64, uint16_t *flags, char* name, uint32_t n
 				case NBD_REP_ERR_POLICY:
 					if(rep->datasize > 0) {
 						char errstr[1024];
-						rep->data[rep->datasize] = '\0';
+						rep->data[rep->datasize - 1] = '\0';
 						snprintf(errstr, sizeof errstr, "Connection not allowed by server policy. Server said: %s", rep->data);
 						err(errstr);
 					} else {
@@ -714,7 +714,7 @@ void negotiate(int *sockp, u64 *rsize64, uint16_t *flags, char* name, uint32_t n
 				default:
 					if(rep->datasize > 0) {
 						char errstr[1024];
-						rep->data[rep->datasize] = '\0';
+						rep->data[rep->datasize - 1] = '\0';
 						snprintf(errstr, sizeof errstr, "Unknown error returned by server. Server said: %s", rep->data);
 						err(errstr);
 					} else {