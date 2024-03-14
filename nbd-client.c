NetworkBlockDevice/nbd

commit 915444bc0b8a931d32dfb755542f4bd1d37f1449

fix port setting from nbdtab
Currently setting port= in nbdtab is accepted, but doesn't take effect.
This change fixes it.

nbd-client.c
@@ -757,6 +757,7 @@ bool get_from_config(char* cfgname, char** name_ptr, char** dev_ptr, char** host
 	*persist = cur_client->persist ? 1 : 0;
 	*swap = cur_client->swap ? 1 : 0;
 	*b_unix = cur_client->b_unix ? 1 : 0;
+	*port = cur_client->port;
 	*num_conns = cur_client->nconn;
 	*certfile = cur_client->cert;
 	*keyfile = cur_client->key;

commit a25f41af1c18c985960070b7c0f1e96f39959fb9

Set sensible default for port
Commit 915444bc0b8a931d32dfb755542f4bd1d37f1449 introduced a regression
whereby an entry in nbdtab with no port specification was read as
wanting port "0" rather than the default "10809".

nbd-client.c
@@ -754,6 +754,7 @@ bool get_from_config(char* cfgname, char** name_ptr, char** dev_ptr, char** host
 	cur_client = calloc(sizeof(CLIENT), 1);
 	cur_client->bs = 512;
 	cur_client->nconn = 1;
+	cur_client->port = NBD_DEFAULT_PORT;
 	yyin = fopen(SYSCONFDIR "/nbdtab", "r");
 	yyout = fopen("/dev/null", "w");
 